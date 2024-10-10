2f17435631af433acd93e49ec36875f85c90019c
Pedro13 <ph.artes13@gmail.com>
2024-10-10 11:08:22 -0300

    projeto eventos

---
+++ src/main/java/com/eventostec/api/domain/event/Event.java
@@ -0,0 +1,21 @@
+package com.eventostec.api.domain.event;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.util.UUID;
+
+@Table(name = "event")
+@Entity
+public class Event {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String title;
+
+    private String description;
+}
